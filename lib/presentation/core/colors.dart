20a9cad5a1af8f7efe5a6e372c8f10681834a844
aexces <aexces@protonmail.com>
2023-05-17 11:47:17 +0530

    Colors updated

---
+++ lib/presentation/core/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  //light theme
+  static const lightPrimaryColor = Colors.deepPurple;
+  static const lightBackgroundColor = Color(0xFFF4F4F4);
+  static const lightShadeColor = Colors.white;
+
+  //dark theme
+  static const darkPrimaryColor = Colors.deepPurple;
+  static const darkBackgroundColor = Color(0xFF1B1C31);
+  static const darkShadeColor = Color(0xFF22233c);
+}
